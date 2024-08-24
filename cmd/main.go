7750d1f10fa8da8160eb3c2a5b1dc4b9c039ad29
Esteban Andrey Gomez A <79619416+unawaretub86@users.noreply.github.com>
2024-08-24 01:38:23 -0500

    Develop (#4)
    * Feature/initial scaffolding (#1)
    
    * adding git actions
    
    * initial scaffolding
    
    * adding pr template
    
    * add dependencies (#3)

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,8 @@
 package main
+
+import "github.com/unawaretub86/leal-challenge/cmd/server"
+
+func main() {
+	initiator := server.NewInitiator()
+	initiator.InitAll()
+}
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,1 +1,74 @@
 package server
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"github.com/unawaretub86/leal-challenge/cmd/server/dependencies"
+	configuration "github.com/unawaretub86/leal-challenge/config"
+	"github.com/unawaretub86/leal-challenge/src/adapters/http"
+	"github.com/unawaretub86/leal-challenge/src/adapters/repository"
+	"github.com/unawaretub86/leal-challenge/src/domain/ports"
+	"github.com/unawaretub86/leal-challenge/src/domain/services"
+)
+
+type Service struct {
+	leal ports.LealPort
+}
+
+type Initiator struct {
+	service Service
+	router  *gin.Engine
+}
+
+func NewInitiator() *Initiator {
+	return &Initiator{
+		service: Service{},
+		router:  &gin.Engine{},
+	}
+}
+
+
+func (i *Initiator) initService() {
+	lealRepo := repository.NewLealRepository(i.initDB())
+	lealService := services.NewLealService(lealRepo)
+
+	i.service = Service{
+		lealService,
+	}
+}
+
+func (i *Initiator) initRouter() {
+	i.router = gin.Default()
+
+	lealRouter := http.NewRouter(i.service.leal)
+	lealRouter.SetRoutes(i.router)
+}
+
+func (i *Initiator) GetRouter() *gin.Engine {
+	return i.router
+}
+
+func (i *Initiator) initDB() *gorm.DB {
+	dbUrl := configuration.GetDatabaseConfig()
+
+	db, err := dependencies.ConnectDB(dbUrl)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db.Connection()
+}
+
+func (i *Initiator) InitAll() {
+	i.initDB()
+	i.initRouter()
+	i.initService()
+
+	err := i.GetRouter().Run(":" + "8080")
+	if err != nil {
+		log.Fatal("Server init error:", err.Error())
+	}
+}
