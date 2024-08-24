6adebc99c5dca38dd246c9ad455bc234af00da49
Esteban Gomez <unawaretub86@gmail.com>
2024-08-24 02:48:21 -0500

    changes

---
+++ src/adapters/http/dto/createUser.go
@@ -0,0 +1,17 @@
+package dto
+
+import (
+	"github.com/unawaretub86/leal-challenge/src/domain/entities/domain"
+)
+
+type CreateUserReq struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func NewUser(userReq CreateUserReq) (domain.User, error) {
+	return domain.User{
+		Name:  &userReq.Name,
+		Email: &userReq.Email,
+	}, nil
+}
