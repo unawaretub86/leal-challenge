8d7c9af69fcf62596c3a1c76e2280f8db940182f
Esteban Gomez <unawaretub86@gmail.com>
2024-10-02 17:31:15 -0500

    domaion clean

--- src/domain/entities/domain/purchase.go
+++ src/domain/entities/domain/purchase.go
@@ -3,14 +3,14 @@ package domain
 import "time"
 
 type Purchase struct {
-	ID             uint64    `gorm:"primaryKey;autoIncrement"`
-	Amount         float64   `json:"amount" validate:"required"`
-	UserID         uint64    `json:"user_id" validate:"required"`
-	CommerceID     uint64    `json:"commerce_id" validate:"required"`
-	BranchID       uint64    `json:"branch_id" validate:"required"`
-	CampaignID     uint64    `json:"campaign_id" validate:"required"`
-	EarnedPoints   int       `json:"earned_points"`
-	EarnedCashBack int       `json:"earned_cashBack"`
-	CreatedAt      time.Time `json:"created_at"`
-	RedeemPoints   bool      `json:"redeem_points" validate:"required"`
+	ID             uint64 `gorm:"primaryKey;autoIncrement"`
+	Amount         float64
+	UserID         uint64
+	CommerceID     uint64
+	BranchID       uint64
+	CampaignID     uint64
+	EarnedPoints   int
+	EarnedCashBack int
+	CreatedAt      time.Time
+	RedeemPoints   bool
 }
