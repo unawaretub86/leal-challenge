ad8e83032b4f07c65f458b1d5aa3831508b1dbaa
Esteban Andrey Gomez A <79619416+unawaretub86@users.noreply.github.com>
2024-10-02 17:32:42 -0500

    Develop (#17)
    * Feature/initial scaffolding (#1)
    
    * adding git actions
    
    * initial scaffolding
    
    * adding pr template
    
    * add dependencies (#3)
    
    * Feature/create user (#5)
    
    * changes
    
    * create user
    
    * Feature/create commerce (#6)
    
    * changes
    
    * create user
    
    * creando branch
    
    * campaing
    
    * refactor dtos
    
    * commerce logic
    
    * fix
    
    * Feature/create purchase (#8)
    
    * changes
    
    * create user
    
    * creando branch
    
    * campaing
    
    * refactor dtos
    
    * commerce logic
    
    * fix
    
    * rm dto
    
    * Develop (#7)
    
    * Feature/initial scaffolding (#1)
    
    * adding git actions
    
    * initial scaffolding
    
    * adding pr template
    
    * add dependencies (#3)
    
    * Feature/create user (#5)
    
    * changes
    
    * create user
    
    * Feature/create commerce (#6)
    
    * changes
    
    * create user
    
    * creando branch
    
    * campaing
    
    * refactor dtos
    
    * commerce logic
    
    * fix
    
    * purchase
    
    * purchase
    
    * Feature/add swagger (#10)
    
    * changes
    
    * adding swaggo
    
    * changes
    
    * User feature (#12)
    
    * register purchase
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * fix
    
    * fix
    
    * rm err
    
    * change
    
    * clean (#14)
    
    * Clean (#16)
    
    * clean
    
    * change
    
    * domaion clean

--- src/domain/entities/domain/branch.go
+++ src/domain/entities/domain/branch.go
@@ -3,12 +3,12 @@ package domain
 import "time"
 
 type Branch struct {
-	ID         uint64     `gorm:"primaryKey"`
-	Name       string     `json:"name" validate:"required"`
-	CommerceID uint64     `json:"commerce_id" validate:"required"`
-	Address    string     `json:"address"`
-	StartDate  time.Time  `json:"start_date"`
-	EndDate    *time.Time `json:"end_date,omitempty"`
-	CreatedAt  time.Time  `json:"created_at"`
-	UpdatedAt  time.Time  `json:"updated_at"`
+	ID         uint64 `gorm:"primaryKey"`
+	Name       string
+	CommerceID uint64
+	Address    string
+	StartDate  time.Time
+	EndDate    *time.Time
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
--- src/domain/entities/domain/commerce.go
+++ src/domain/entities/domain/commerce.go
@@ -3,10 +3,8 @@ package domain
 import "time"
 
 type Commerce struct {
-	ID        uint64     `gorm:"primaryKey"`
-	Name      string     `json:"name" validate:"required"`
-	Points    []Points   `gorm:"foreignKey:CommerceID"`
-	Cashbacks []Cashback `gorm:"foreignKey:CommerceID"`
+	ID        uint64 `gorm:"primaryKey"`
+	Name      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
